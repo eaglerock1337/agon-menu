6abf97dcc754dc8a4a8054d1169396fd65adb677
Peter Marks <pmarks@signiant.com>
2023-10-27 12:47:11 -0400

    add breakintoprogram libs and WIP Makefile

--- include/common.h
+++ include/common.h
@@ -1,7 +1,30 @@
-#ifndef _COMMON_HEADER_
-#define _COMMON_HEADER_
+#ifndef _COMMON_H_
+#define _COMMON_H_
 
-typedef	unsigned char	u8;
-typedef	unsigned int	u16;
+// Macros for disabling/enabling interrupts
+#define DI() 		asm( "\tdi" )
+#define EI() 		asm( "\tei" )
+#define	di()		DI()
+#define ei()		EI()
 
-#endif 
+// Zilog standard data types
+typedef void VOID ;
+typedef char CHAR ;
+typedef unsigned char UCHAR;
+typedef unsigned char BYTE ;
+typedef	unsigned char BOOL;
+typedef char INT8;
+typedef unsigned char UINT8 ;
+typedef short INT16 ;
+typedef unsigned short UINT16 ;
+typedef unsigned short WORD;
+typedef int INT24 ;
+typedef int INT ;
+typedef unsigned int UINT ;
+typedef unsigned int UINT24;
+typedef long INT32;
+typedef unsigned long UINT32 ;
+typedef unsigned long DWORD;
+typedef float FLOAT32 ;
+
+#endif
--- src/menu.c
+++ src/menu.c
@@ -3,7 +3,7 @@
 char w[] = "Hello, alex!\r\n";
 
 void printMe() {
-    printf("Hello, world!\r\n%s\nPTR is: %x\r\n", w, w);
+    printf("Hello, world!\r\n%s\nPTR is: %p\r\n", w, w);
     
     char c = getchar();
     printf("Char code: %x\r\n", c);
